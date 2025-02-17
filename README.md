fb5d0875b25d1c93b075fd70500e00f54dba072b
Muhammad Rauf <amahi@mail.stmarytx.edu>
2025-02-17 15:44:39 -0600

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # End-to-End-Medical-Chatbot-Generative-AI
+
+# How to Run? 
+### Steps 
+
+Clone The Repo
+'''bash 
+Project repo: https://github.com/
+
+### Step 01: Create a conda environment after opeening the repository 
+'''bash 
+conda create -n medibot python=3.10 -y
+''''
+
+'''bash 
+conda activate medibot
+
+### STEP 02- install the requirements
+'''bash 
+pip install -r requirements.txt
+'''
+
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI Project",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "sentence-transformers==2.2.2",
+        "flask",
+        "pypdf",
+        "python-dotenv",
+        "pinecone[grpc]",
+        "langchain-pinecone",
+        "langchain_community",
+        "langchain_openai",
+        "langchain_experimental",
+    ],
+)
